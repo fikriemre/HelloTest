9c940897d4e48d36d4907f8f75d6529f9d6d9019
fikriemre <fikriemre@gmail.com>
2018-04-05 18:23:34 +0300

    asd
    asd

---
+++ Hello/Hello.cpp
@@ -0,0 +1,17 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace::std;
+
+int main()
+{
+
+		cout <<" helloworld";
+		int a;
+		cin >> a;
+
+    return 0;
+}
+
